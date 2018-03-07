5e1d7841a468014487b9d58e18483856c6a5029c
Даниил Горшков <daniil.gorshkov@lakehouse.ru>
2018-03-07 15:03:46 +0300

    add option through has_and_belongs_to_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 
-	 has_and_belongs_to_many :books
+	 has_and_belongs_to_many :books, through: :ordered_books
 
-  devise	:database_authenticatable, :registerable,
-    			:recoverable, :rememberable, :trackable, :validatable
+  devise	:database_authenticatable, :registerable, :recoverable,
+  				:rememberable, :trackable, :validatable
 
   validates :name, presence: true
   validates :surname, presence: true
