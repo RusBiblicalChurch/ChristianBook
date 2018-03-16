4971a94ad96faf0ef70bb188b1a17bbc1f724b79
Даниил Горшков <daniil.gorshkov@lakehouse.ru>
2018-03-16 15:54:23 +0300

    add adctivadmin

---
+++ app/admin/books.rb
@@ -0,0 +1,24 @@
+ActiveAdmin.register Book do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# has_many :users, through: :ordered_books
+permit_params :title, :description, :recommended_church
+
+index do
+    selectable_column
+    id_column
+    column :title
+    column :description
+    column :recommended_church
+    column :created_at
+    actions
+end 
+
+filter :title
+filter :description
+filter :recommended_church
+filter :created_at
+
+
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
-
-  has_and_belongs_to_many :users, through: :ordered_books
+	
+	has_many :ordered_books
+  has_many :users, through: :ordered_books
 
 end
