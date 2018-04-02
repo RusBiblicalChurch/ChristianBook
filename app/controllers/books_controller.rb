f5ba029b36ccf6bfa14018dd1e2de47417177f93
Даниил Горшков <daniil.gorshkov@lakehouse.ru>
2018-04-02 17:17:27 +0300

    fix rubocop

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     respond_to do |format|
-    	format.html
+      format.html
       format.json { render json: @books }
     end
   end
