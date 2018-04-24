9eb7a4f47744b29137ea7cb1d92de2ee931fad98
isabloCh <rock132dmj@gmail.com>
2018-04-24 20:24:02 +0300

    added error handling, fix remarks

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -2,8 +2,8 @@ module Api
   class BooksController < ActionController::Base
 
     def index
-      @books = Book.all
-      render json: @books
+      books = Book.all
+      render json: books
     end
 
   end
--- app/javascript/vue/books-data.js
+++ app/javascript/vue/books-data.js
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
 export default new Promise((resolve) => {
-  axios('/api/books').then((response) => resolve(response.data) )
+  axios('/api/books')
+    .then((response) => resolve(response.data))
+    .catch(function (error) {
+      alert('ошибонька вышла');
+      console.log(error);
+    })
 })
