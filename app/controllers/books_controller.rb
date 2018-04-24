49b5b35344c467c9faa88ed24e5d9adaaef4a590
isabloCh <rock132dmj@gmail.com>
2018-04-24 13:16:31 +0300

    deleted file index.html.erb to dooks controller, deleted method index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,2 @@
 class BooksController < ApplicationController
-
-  def index
-    @books = Book.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @books }
-    end
-  end
-
 end
