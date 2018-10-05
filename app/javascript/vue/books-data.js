f38c4970ecd554db78354f05ea26854fd3a86a78
Dany <rock132dmj@gmail.com>
2018-10-05 13:27:13 +0300

    Expected the Promise rejection reason to be an Error

--- app/javascript/vue/books-data.js
+++ app/javascript/vue/books-data.js
@@ -13,12 +13,11 @@ function getById (id) {
   return new Promise((resolve, reject) => {
     getAll()
       .then((books) => {
-        let error = "Can't find book with ID: "
         const book = _.find(books, { id: id })
         if (book) {
           resolve(book)
         } else {
-          reject(error + id)
+          reject(new Error('Can`t find book with ID'));
         }
       })
       .catch((e) => reject(e))
